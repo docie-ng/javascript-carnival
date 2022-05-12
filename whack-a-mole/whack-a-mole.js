2707471fb9558684662923ac099e8ff0150946f9
Docie Ng <docieng@Docies-MacBook-Air.local>
2022-05-12 13:27:06 +1200

    create function for random cell

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,25 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let holes = document.getElementsByTagName('TD')
+
+// for (let i = 0; i < holes.length; i++) {
+//   holes[i].onclick = whack
+// }
+// // checking for clicks
+// function whack(e) {
+//   let holes = e.target
+//   console.log('whack ')
+// }
+
+// Randomly put the mole inside
+
+function putMoleinHole() {
+  let randomIndex = Math.floor(Math.random() * 24)
+  let holes = holes[randomIndex]
+  console.log(randomIndex)
+}
+putMoleinHole()
